396b6329e16474c76b59295c190dbde4e102e76c
Janetmaria <janetsaji1652@gmail.com>
2025-06-09 13:36:18 +0000

    Level3 Commit

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -11,10 +11,7 @@ class Task extends React.Component<TaskProp> {
     render() {
     return (
       <div className="TaskItem shadow-md border border-slate-100">
-        <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-        <p className="text-sm text-slate-500">
-          Due Date: {this.props.dueDate}
-        </p>
+        <h2 className="text-base font-bold my-1">{this.props.title} className="text-sm text-slate-500" ({this.props.dueDate})</h2>
         <p className="text-sm text-slate-500">
           Description: {this.props.description}
         </p>
