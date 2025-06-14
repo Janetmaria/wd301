3f87e2db4abfaa585ca15837248919d71e3af460
Janetmaria <janetsaji1652@gmail.com>
2025-06-14 13:13:50 +0000

    Level7 commit

---
+++ smarter-tasks/src/context/members/reducer.ts
@@ -0,0 +1,62 @@
+interface Member {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface MembersSate {
+  members: Member[];
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+}
+
+export const initialState: MembersSate = {
+  members: [],
+  isLoading: false,
+  isError: false,
+  errorMessage: ''
+};
+
+export type MembersActions = 
+  | { type: 'FETCH_MEMBERS_REQUEST' }
+  | { type: 'FETCH_MEMBERS_SUCCESS'; payload: Member[] }
+  | { type: 'FETCH_MEMBERS_FAILURE'; payload: string }
+  | { type: 'ADD_MEMBERS_SUCCESS'; payload: Member }
+  | { type: 'DELETE_MEMBER_SUCCESS'; payload: number };
+
+
+export const reducer = (state: MembersSate, action: MembersActions): MembersSate => {
+  switch (action.type) {
+    case "FETCH_MEMBERS_REQUEST":
+      return {
+        ...state,
+        isLoading: true
+      };   
+    case "FETCH_MEMBERS_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        members: action.payload,
+      };      
+    case "FETCH_MEMBERS_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true, 
+        errorMessage: action.payload
+      };           
+    case 'ADD_MEMBERS_SUCCESS':
+      return {
+        ...state, 
+        members: [...state.members, action.payload] 
+      }; 
+    case 'DELETE_MEMBER_SUCCESS':
+      return {
+        ...state,
+        members: state.members.filter(member => member.id !== action.payload)
+      };
+    default:
+      return state;
+  }
+}
---
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import { useMembersState, useMembersDispatch } from "../../context/members/context";
+import { deleteMember } from "../../context/members/action";
+
+export default function MemberListItems() {
+
+  // eslint-disable-next-line prefer-const
+  let state: any = useMembersState();
+  const dispatchMembers = useMembersDispatch();
+
+  const { members, isLoading, isError, errorMessage } = state
+  console.log(members);
+
+  if (members.length === 0 && isLoading) {
+    return <span>Loading...</span>;
+  }
+  
+  if (isError) {
+    return <span>{errorMessage}</span>;
+  }
+
+  return (
+    <>
+      {members.map((member: any) => (
+        <div key={member.id} className="member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
+          <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.email}</h5>
+          <button className="cursor-pointer h-4 w-4 rounded-full my-5 mr-5"
+          onClick={(event) => {
+            event.preventDefault();
+            deleteMember(dispatchMembers, member.id)
+          }}>Delete
+          </button>
+        </div>
+      ))}        
+    </>
+  );
+}
+        
