aa5e633776271707d955b2f54c36ffac33c65e6f
Janetmaria <janetsaji1652@gmail.com>
2025-06-14 13:41:05 +0000

    Level7 commit

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
-    const navigate = useNavigate();
     return (
         <div>
         <nav>Navigation</nav>
