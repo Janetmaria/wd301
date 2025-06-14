6eb59b1512094f3a0925438c4bb85f83364b15ae
Janetmaria <janetsaji1652@gmail.com>
2025-06-14 14:25:18 +0000

    Level7 commit

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -24,8 +24,8 @@ export default function MemberListItems() {
     <>
       {members.map((member: any) => (
         <div key={member.id} className="member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.email}</h5>
+          <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
           <button className="cursor-pointer h-4 w-4 rounded-full my-5 mr-5"
           onClick={(event) => {
             event.preventDefault();
